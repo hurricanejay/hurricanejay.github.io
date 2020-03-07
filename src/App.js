56937c886a4ee524c6784db9bf0f8dba4b6b8ac8
hurricanejay <jaywen89@gmail.com>
2020-03-07 13:47:55 -0500

    adds initial set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { 
+    title: "Jay Wen", 
+    headerLinks: [ 
+      {title: "Home", path: '/'},
+      {title: "About", path: '/About'}, 
+      {title: "Contact", path: '/Contact'} 
+    ],  
+
+    home: {
+      title: "Aesthetics & Functionality",
+      subtitle: "A happy partnership",
+      text: "My latest projects..."
+    },
+
+    about: {
+      title: "About",
+    },
+
+    contact: {
+      title: "Say Hi!",
+    }
+
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>jay's portfolio</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
