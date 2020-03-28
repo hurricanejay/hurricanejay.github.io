e187227fcf2c6bb1103958b1bef069f9d395579f
hurricanejay <jaywen89@gmail.com>
2020-03-28 17:35:13 -0400

    Updates

---
+++ precache-manifest.f980418626ab7e3a7bfb1312d694ed04.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d8246697ca61e927d1b1280c2fb753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f20d83dc59d9d490f243",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "91e3319afa0722db83db",
+    "url": "/static/css/main.16787939.chunk.css"
+  },
+  {
+    "revision": "f20d83dc59d9d490f243",
+    "url": "/static/js/2.fc99783b.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.fc99783b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e3319afa0722db83db",
+    "url": "/static/js/main.120a6580.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "9f33ec70f386277b7b79e9c93565c8c0",
+    "url": "/static/media/heirloom_salad.9f33ec70.png"
+  },
+  {
+    "revision": "b068a4217ac641f254cbf02ee9cb2edd",
+    "url": "/static/media/ice_cream.b068a421.jpg"
+  },
+  {
+    "revision": "f3684128ee25289b04546c86e7c7c43e",
+    "url": "/static/media/townhouse.f3684128.JPG"
+  }
+]);
