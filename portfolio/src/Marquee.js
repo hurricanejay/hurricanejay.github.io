4e4a17c585f693a3d6496a9c29b73934218cb00f
Jay Wen <jaywen89@gmail.com>
2022-09-05 13:37:39 -0400

    portfolio update

---
+++ portfolio/src/Marquee.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Marquee = () => {
+  return (
+    <div class="marquee">
+    <div class="track">
+      <div class="content wavy-underline"> 
+       ~ currently open to new opportunities  ~   currently open to new opportunities ~  currently open to new opportunities~ currently open to new opportunities  ~
+      </div>
+    </div>
+  </div>
+  )
+}
